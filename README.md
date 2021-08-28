6fd3e7ba5a8fb0642393741d93e41d6dd3b8d70f
한우진 <68366527+dnwls3023@users.noreply.github.com>
2021-08-28 07:56:01 +0000

    tmp

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # 백준 알고리즘 시즌 2
 
-# 파일 설명 boj 폴더에 src 폴더와
+# 파일 사용방법 : boj 폴더에 src와 description 폴더가 있습니다.
 
 # src 파일 : 소스코드 모음
 
-#   : 설명 모음
+# description 파일 : 설명 모음
--- boj/src/boj+2231.cpp
+++ boj/src/boj+2231.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include <string>
+#define INF 987654321 
 using namespace std;
 
+int n;
+
+int partitionSum(int n){
+  int ret = 0;
+  string s = to_string(n);
+  int length = s.size();
+  for(int i = 0 ; i< length;++i){
+    ret += s[i]-'0';
+  }
+  return ret;
+}
+
 int main(void){
-  
+  cin>>n;
+  string str = to_string(n);
+  int length = str.size();
+
+  int minimum = INF;
+
+  for(int i = 1 ; i<=n;++i){
+    if()
+    minimum = min(minimum, partitionSum(i)+i);
+  }
+
+  int answer = minimum;
+
+  cout<< answer <<endl;
+
   return 0;
 }
